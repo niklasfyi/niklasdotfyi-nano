98638ee0b5eb273406909497559d994ea8025d12
Niklas Siefke <niklas@mazemap.com>
2024-07-04 18:16:29 +0200

    Temporary change to vercel domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.niklas.fyi",
+  // site: "https://www.niklas.fyi",
+  site: "https://niklasdotfyi-nano.vercel.app",
   integrations: [mdx(), sitemap(), tailwind(), icon()]
 });
