0a316e85068c5ffea42342c71cd29e9a7e4b8da7
Niklas Siefke <niklas@mazemap.com>
2024-07-04 18:04:32 +0200

    Add og image

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-import type { Site, Metadata, Socials } from "@types";
+import type { Site, Metadata } from "@types";
 
 export const SITE: Site = {
   NAME: "Niklas Siefke",
@@ -15,22 +15,3 @@ export const BLOG: Metadata = {
   TITLE: "Blog",
   DESCRIPTION: "A collection of posts on topics I am passionate about.",
 };
-
-export const SOCIALS: Socials = [
-  {
-    NAME: "mastodon",
-    HREF: "https://hachyderm.io/@niklasfyi",
-  },
-  {
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/niklasfyi",
-  },
-  {
-    NAME: "github",
-    HREF: "https://github.com/niklasfyi"
-  },
-  {
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/niklas-%F0%9F%92%BB-siefke-70448455/",
-  }
-];
